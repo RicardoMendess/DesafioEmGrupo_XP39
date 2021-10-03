70d539145fec2d7a3d4ec0401af8c02944135dc4
Ricardo Mendes <ricardomendesprofissional@gmail.com>
2021-10-02 22:16:10 -0300

    Projeto Javascript Atualizado

--- resoluçao.js
+++ resoluçao.js
@@ -1,5 +1,5 @@
 // Resolução da primeira questão:
-function Listaprodutos (listaProdutos) {
+function exercicio01 (listaProdutos) {
     var lista = 0;
     for (var i = 0;i < listaProdutos.length; i++) {
         lista += listaProdutos[i].qtdEstoque;  
@@ -7,8 +7,55 @@ function Listaprodutos (listaProdutos) {
     return lista;
 }
 
-var primeiraLista = Listaprodutos(listaProdutos);
+var primeiraLista = exercicio01 (listaProdutos);
 
 console.log(primeiraLista + " " + "produtos encontrados.");
 
 // Resolução da segunda questão: 
+
+function exercicio02 (listaProdutos, yes) {
+    var destaque = [];
+    for (var i = 0;i < listaProdutos.length; i++) {
+        if (listaProdutos[i].emDestaque === yes) {
+            destaque.push(listaProdutos[i]);
+        }
+    }
+    return destaque;
+}
+
+var segundaLista = exercicio02 (listaProdutos, "sim");
+
+console.log (segundaLista.length + " " + "produtos encontrados.")
+
+// Resolução da terceira questão:
+
+function exercicio03 (listaProdutos, yes) {
+    var dispo = [];
+
+    for (var i = 0; i < listaProdutos.length; i++) {
+        if (listaProdutos[i].disponivel === yes) {
+            dispo.push(listaProdutos[i]);
+        }
+    }
+    return dispo;
+}
+
+var terceiraLista = exercicio03 (listaProdutos, "sim");
+
+console.log (terceiraLista.length + " " + "produtos encontrados.");
+
+// Resolução da quarta questão
+
+function exercicio04 (listaProdutos, yes) {
+    var presente = [];
+
+    for (var i = 0; i < listaProdutos.length; i++) {
+        if (listaProdutos[i].disponivel === yes && listaProdutos[i].emDestaque === yes) {
+            presente.push(listaProdutos[i]);
+        }
+    }
+    return presente;
+}
+
+var quartaLista = exercicio04 (listaProdutos, "sim");
+console.log (quartaLista.length + " " + "produtos encontrados.")
